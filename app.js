de5f4d47a2f8998e8cbf6b020fd733f986c173f0
Mohammed Waleed <mhmd.waleed@outlook.com>
2021-04-23 13:43:08 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.urlencoded({
+  extended: false,
+}));
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+const server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
+});
